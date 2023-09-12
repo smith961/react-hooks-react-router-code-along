d13a9d04878413df081b59183367ddaa127e0e11
unknown <smithbabawande@gmail.com>
2023-09-12 09:03:44 +0100

    Completed Assignment

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import React from "react";
 import ReactDOM from "react-dom";
+import { BrowserRouter, Route } from "react-router-dom";
 
 function Home() {
   return (
@@ -10,7 +11,50 @@ function Home() {
 }
 
 function App() {
-  return <Home />;
+  return (
+    <div>
+      <Route exact path="/">
+        <Home />
+      </Route>
+      <Route path="/about">
+        <About />
+      </Route>
+      <Route path="/login">
+        <Login />
+      </Route>
+    </div>
+  )
 }
 
-ReactDOM.render(<App />, document.getElementById("root"));
+function About() {
+  return (
+    <div>
+      <h1>This is my about component</h1>
+    </div>
+  )
+}
+
+function Login() {
+  return (
+    <div>
+      <h1>Login</h1>
+      <form>
+        <div>
+          <input type="text" name="username" placeholder="username" />
+        </div>
+        <div>
+          <input type="password" name="password" placeholder="password" />
+        </div>
+        <div>
+          <input type="submit" value="submit" />
+        </div>
+      </form>
+    </div>
+  )
+}
+
+ReactDOM.render(
+  <BrowserRouter>
+    <App />
+  </BrowserRouter>,
+  document.getElementById("root"));
